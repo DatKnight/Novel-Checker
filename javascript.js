d19e2228655e232f77a3ee6a30200844d5027a35
DatKnight <romano@hotmail.com.hk>
2018-01-31 15:15:02 +0000

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -9,7 +9,6 @@ Date.prototype.timeNow = function () {
 }
 
 function loadDataRR(url, id){
-  var result = null;
   var origin = 'https://allorigins.me/get?url=' +
   encodeURIComponent(url) +
   'callback=?';
@@ -17,11 +16,53 @@ function loadDataRR(url, id){
     var array = response.contents.match(/[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{1,4}\<\/time> ago/g);
     var num = array.length;
     var lastUpdate = array[num - 1].match(/[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{1,4}/)[0];
-    updateField(id,num + ' ' + lastUpdate);
+    updateField(id,num,lastUpdate);
   });
 }
 
-function updateField(id,content){
+function loadDataAO3(url, id){
+  var origin = 'https://allorigins.me/get?url=' +
+  encodeURIComponent(url) +
+  'callback=?';
+  $.get(origin, function(response){
+    var array = response.contents.match(/Updated:.+"chapters"\>[0-9]{1,3}/);
+    var num = array[0].match(/"chapters"\>[0-9]{1,3}/)[0];
+    var num = num.match(/[0-9]{1,3}/)[0];
+    var lastUpdate = array[0].match(/[0-9]{1,2}-[0-9]{1,2}-[0-9]{1,4}/)[0]
+    updateField(id,num,lastUpdate);
+  });
+}
+
+function loadDataFP(url, id){
+  var origin = 'https://allorigins.me/get?url=' +
+  encodeURIComponent(url) +
+  'callback=?';
+  $.get(origin, function(response){
+    var array = response.contents.match(/<span class="xgray xcontrast_txt"\> == \$0 .+ Published:/);
+    console.log(array);
+    //var num = array.length;
+    //var lastUpdate = array[num - 1].match(/[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{1,4}/)[0];
+    //updateField(id,num + ' ' + lastUpdate);
+  });
+}
+
+function loadDataGeneric(url, id, regex){
+  var origin = 'https://allorigins.me/get?url=' +
+  encodeURIComponent(url) +
+  'callback=?';
+  $.get(origin, function(response){
+    var array = response.contents.match(regex);
+    var num = array[0];
+    updateField(id,num);
+  });
+}
+
+function updateField(id,chapter,date){
+  current = document.getElementById(id);
+  document.getElementById(id).innerHTML = 'Current Chapter: ' + chapter + ' Last Updated: ' + date;
+}
+
+function updateDateField(id,content){
   current = document.getElementById(id);
   document.getElementById(id).innerHTML = content;
 }
@@ -34,7 +75,8 @@ function getDateTime(){
 function checkUpdates(){
   loadDataRR('http://royalroadl.com/fiction/8894/everybody-loves-large-chests', 'ELLC');
   loadDataRR('http://royalroadl.com/fiction/5701/savage-divinity', 'SD');
-  updateField('lastTime',getDateTime());
+  loadDataAO3('http://archiveofourown.org/works/11478249/chapters/25740126','WTC');
+  updateDateField('lastTime',getDateTime());
 }
 
 window.onload = checkUpdates();
